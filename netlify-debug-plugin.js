71dceb9f8c5835cfb92d9b750c6ad87e44f75be1
Muhammad Saeed <qasimgondle+github@gmail.com>
2025-09-09 20:47:02 +0500

    fixing netlify issues

--- netlify-debug-plugin.js
+++ netlify-debug-plugin.js
@@ -2,8 +2,13 @@ module.exports = {
   onPreBuild: ({ utils }) => {
     console.log('Netlify Build Starting...');
     console.log('Node version:', process.version);
+    console.log('NODE_VERSION env:', process.env.NODE_VERSION);
     console.log('Current directory:', process.cwd());
     console.log('Directory contents:', utils.run.command('ls -la'));
+    console.log(
+      'NVM status:',
+      utils.run.command('which node || echo "No node found"')
+    );
     console.log('Available npm scripts:', utils.run.command('npm run'));
   },
   onBuild: ({ utils }) => {
